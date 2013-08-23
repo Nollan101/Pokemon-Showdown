8a187bb6a9aa7c70ec690c41bddd47075b549757
Nollan101 <kgb@kgbudge.com>
2013-08-23 10:53:51 -0600

    Update scripts.js

--- mods/typeswap/scripts.js
+++ mods/typeswap/scripts.js
@@ -2,9 +2,15 @@ exports.BattleScripts = {
     init: function() {
         for (var i in this.data.Pokedex) {
             if (this.data.Pokedex.types[0] === 'Water') {
-                return this.data.Pokedex.types[0] = 'Grass';
+                this.modData.('Pokedex', i).types[0] = 'Grass';
             } else if (this.data.Pokedex.types[1] === 'Water') {
-                return this.data.Pokedex.types[1] = 'Grass';
+                this.modData.('Pokedex', i).types[1] = 'Grass';
+            }
+        }
+        for (var x in this.data.Learnsets) {
+            if (this.data.Learnsets[x].waterfall) {
+                delete this.modData.('Learnsets', x).waterfall;
+                this.modData.('Learnsets', x).seedbomb = ['5L0'];
             }
         }
     }
